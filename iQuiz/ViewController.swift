e3481c181177e982fd02fdcc09d94aba95e6bd96
Linconl Rufino <linconldasilvarufino@gmail.com>
2023-06-30 17:27:20 -0300

    Initial Commit

---
+++ iQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iQuiz
+//
+//  Created by Linconl Rufino on 30/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
