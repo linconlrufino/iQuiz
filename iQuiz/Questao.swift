e2f1d90e0640f05f3ac92807076661aa364d8042
Linconl Rufino <linconldasilvarufino@gmail.com>
2023-07-04 00:38:02 -0300

    Próxima questão e alterando cor de fundo do botão

--- iQuiz/Questao.swift
+++ iQuiz/Questao.swift
@@ -17,5 +17,5 @@ let questoes: [Questao] = [
     Questao(titulo: "Qual feitiço para desarmar o seu oponente, em Harry Potter?", respostas: ["Expecto Patronum", "Avada Kedavra", "Expelliarmus"], respostaCorreta: 2),
     Questao(titulo: "Em que ano Vingadores Ultimato foi lançado?", respostas: ["2019", "2018", "2017"], respostaCorreta: 0),
     Questao(titulo: "Quando foi lançado The Last of Us?", respostas: ["2016", "2013", "2014"], respostaCorreta: 1),
-    Questao(titulo: "Quem foi é a Frida?", respostas: ["Uma pintora mexicana", "Um gato rajado", "Uma Lutadora de MMA"], respostaCorreta: 1)
+    Questao(titulo: "Quem foi Frida?", respostas: ["Uma pintora mexicana", "Um gato rajado", "Uma Lutadora de MMA"], respostaCorreta: 1)
 ]
--- iQuiz/QuestaoViewController.swift
+++ iQuiz/QuestaoViewController.swift
@@ -20,7 +20,14 @@ class QuestaoViewController: UIViewController {
     
         if usuarioAcertouResposta {
             pontuacao += 1
-            print("O Usuario acertou!")
+            sender.backgroundColor = UIColor(red: 11/255, green: 161/255, blue: 53/255, alpha: 1)
+        } else {
+            sender.backgroundColor = UIColor(red: 211/255, green: 17/255, blue: 17/255, alpha: 1)
+        }
+        
+        if numeroQuestao < questoes.count - 1 {
+            numeroQuestao += 1
+            configurarQuestao()
         }
     }
     
@@ -47,6 +54,7 @@ class QuestaoViewController: UIViewController {
         for botao in botoesRespostas {
             let tituloBotao = questoes[numeroQuestao].respostas[botao.tag]
             botao.setTitle(tituloBotao, for: .normal)
+            botao.backgroundColor = UIColor(red: 116/255, green: 50/255, blue: 255/255, alpha: 1)
         }
     }
 
