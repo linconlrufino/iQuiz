75daf86dc0d932d321ebc381d44c7fc7a7c966ac
Linconl Rufino <linconldasilvarufino@gmail.com>
2023-07-05 21:31:29 -0300

    Conectando os elementos do Storyboard

--- iQuiz/DesempenhoViewController.swift
+++ iQuiz/DesempenhoViewController.swift
@@ -9,13 +9,23 @@ import UIKit
 
 class DesempenhoViewController: UIViewController {
 
+    @IBOutlet weak var resultadoLabel: UILabel!
+    
+    @IBOutlet weak var percentualLabel: UILabel!
+    
+    @IBOutlet weak var botaoReiniciarQuiz: UIButton!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        configurarLayout()
     }
     
-
+    func configurarLayout() {
+        botaoReiniciarQuiz.layer.cornerRadius = 12.0
+    }
+    
     /*
     // MARK: - Navigation
 
